75fcaa7092351ee272aa617d268e2b0aac74f607
HarbeyCely <hecm-09@hotmail.com>
2022-12-10 23:58:49 -0500

    update

--- Estadística/Clase8/TablaNumVSCat.R
+++ Estadística/Clase8/TablaNumVSCat.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(readxl)
 df <- read_excel("C:/Users/CENTIC/Escritorio/Clase8/Datos.xlsx", 
                     col_types = c("numeric", "numeric", "numeric", 
@@ -21,27 +20,3 @@ crosstable(df, c(Q, P ), by=Sexo) %>%
 
 crosstable(df, c(Edad), by=c(ID,Sexo)) %>%
   as_flextable(keep_id=TRUE)
-=======
-library(readxl)
-df <- read_excel("C:/Users/CENTIC/Escritorio/Clase8/Datos.xlsx", 
-                    col_types = c("numeric", "numeric", "numeric", 
-                                  "text", "numeric", "numeric"))
-
-attach(df)
-names(df)
-
-colnames(df) [c(1,2,3,4,5,6)]=c("ID", "Horas","Edad", "Sexo", "Q","P")
-                                          #   Q: Cantidad
-                                          #   P: Precio
-library(crosstable)
-
-class(df$Sexo)
-
-class(df$Q)
-
-crosstable(df, c(Q, P ), by=Sexo) %>%
-  as_flextable(keep_id=TRUE)
-
-crosstable(df, c(Edad), by=c(ID,Sexo)) %>%
-  as_flextable(keep_id=TRUE)
->>>>>>> 5edba337ab19ac039ae761622a22922ce51fc3c1
