0cc2299623f21bfd2e850592d083fbd1f09bc4d1
HarbeyCely <hecm-09@hotmail.com>
2022-12-10 23:58:49 -0500

    update

--- Estadística/clase6/TablasDeContingencia.R
+++ Estadística/clase6/TablasDeContingencia.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(readxl)
 df <- read_excel("C:/Users/CENTIC/Escritorio/clase6/DF.xlsx", 
                  sheet = "Hoja1")
@@ -19,25 +18,3 @@ CrossTable(cerveza, sexo, prop.c = F, prop.t = F,
 
 CrossTable(cerveza, sexo, prop.r = F, prop.t = F,
            expected = F, prop.chisq = F)  # Contingencia relativa Columna
-=======
-library(readxl)
-df <- read_excel("C:/Users/CENTIC/Escritorio/clase6/DF.xlsx", 
-                 sheet = "Hoja1")
-attach(df)
-names(df)
-colnames(df)= c("Sexo", "Cerveza")    # C sirve para declarar un vector
-attach(df)
-names(df)
-## tabla de contingencia sin paquete
-t1=table(sexo, cerveza)
-t1
-prop.table(t1,1) # relativa fila
-prop.table(t1,2) # relativa Columna
-
-library(gmodels)
-CrossTable(cerveza, sexo, prop.c = F, prop.t = F,
-           expected = F, prop.chisq = F)  # Contingencia relativa fila
-
-CrossTable(cerveza, sexo, prop.r = F, prop.t = F,
-           expected = F, prop.chisq = F)  # Contingencia relativa Columna
->>>>>>> 5edba337ab19ac039ae761622a22922ce51fc3c1
