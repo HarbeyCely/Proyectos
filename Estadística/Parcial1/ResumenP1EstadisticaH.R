07b631a28abcc6dfb1280bb02fae33ea3ca9d655
HarbeyCely <86800843+HarbeyCely@users.noreply.github.com>
2022-12-15 11:57:28 -0500

    Update ResumenP1EstadisticaH.R

--- Estadística/Parcial1/ResumenP1EstadisticaH.R
+++ Estadística/Parcial1/ResumenP1EstadisticaH.R
@@ -71,3 +71,24 @@ library(moments)
 skewness(Saber11Mate,na.rm=T)
 #coeficiente de curtosis (vertical)
 kurtosis(Saber11Mate,na.rm=T)
+
+## PUNTO2
+#En la tabla se puede ver que, aunque en varios departamentos el porcentaje de
+#municipios clasificados como Zonas futuro es 0%, no todos lo son como es el caso
+#de Nariño donde el porcentaje de municipios clasificados como Zonas futuro es de
+#12,5 %, o el caso de Norte de Santander con 8 municipios clasificados como Zonas
+#futuro y eso equivale al 20% de sus municipios o un caso extremo como Guaviare
+#donde el 75% de sus municipios es considerado Zona futuro.
+#Por tanto, se puede decir que la afirmación es errónea ya que no todos los
+#Departamentos tienen el mismo porcentaje de sus municipios clasificados como
+#Zonas Futuro.
+
+## PUNTO4
+#Para hacer Muestreo aleatorio Simple se deben seguir los siguientes pasos:
+#1) En una maquina o Software designar un número aleatorio a todas las personas o posibles encuestados.
+#2) Una vez hecho esto Ordenar de mayor a menor los números aleatorios asignados a cada persona
+#3) Finalmente se selecciona a las personas que han quedado con los números mayores de acuerdo con la cantidad
+#de personas que va a tener la muestra, este dato de personas que va a tener la muestra ya se conoce previamente.
+#Es decir que si tengo 100 potenciales encuestados pero solo puedo encuestar a 10 entonces escojo a las 10 personas
+#que quedaron con los 10 números mayores.
+
